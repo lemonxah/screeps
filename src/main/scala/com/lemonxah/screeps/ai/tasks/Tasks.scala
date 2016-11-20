bd808dd281293ef5648fea20bb48b3fb30ffb3ee
lemonxah <lemon.xah@gmail.com>
2016-11-20 18:10:35 +0200

    cant use scala memory have to use Memory object

--- src/main/scala/com/lemonxah/screeps/ai/tasks/Tasks.scala
+++ src/main/scala/com/lemonxah/screeps/ai/tasks/Tasks.scala
@@ -20,6 +20,65 @@ object Tasks {
   implicit def dynamicToBoolean(v: js.Dynamic): Boolean = v.toString.toBoolean
   implicit def dynamicToInt(v: js.Dynamic): Int = v.toString.toInt
 
+  def manageCreeps(count: Int) = Memory.flatMap { mem ⇒
+    Game.map { game ⇒
+      // checking workers for dedcreps
+      if (!js.isUndefined(mem.workers)) {
+        val workers = mem.workers.as[js.Dictionary[Creep]]
+        workers.foreach {
+          case (name, creep) ⇒
+            if (!game.creeps.contains(name))
+              mem.workers.as[js.Dictionary[Creep]].delete(name)
+        }
+      }
+
+      val spawn = game.spawns.values.toList.sortWith(_.energy > _.energy).head
+      if(spawn.spawning == null) {
+        // spawned a creep and need to add it to a list
+        if (!js.isUndefined(mem.newname) && mem.newname.toString != "") {
+          println(s"adding ${mem.newname} to workers")
+          val name: String = mem.newname.toString
+          mem.newname = ""
+          if (js.isUndefined(mem.workers)) {
+            mem.workers = js.Dictionary((name, game.creeps(name)))
+          } else {
+            mem.workers.as[js.Dictionary[Creep]].put(name, game.creeps(name))
+          }
+        }
+        if (js.isUndefined(mem.workers) || mem.workers.as[js.Dictionary[Creep]].keys.size < count) {
+          if (spawn.energy > 200) {
+            val n = spawn.createCreep(js.Array(WORK, CARRY, MOVE))
+            println(s"spawning $n")
+          } else {
+            println(s"waiting for energy on spawner: ${spawn.name}")
+          }
+        }
+      } else {
+        mem.newname = spawn.spawning.name
+        println(s"waiting spawn of ${spawn.spawning.name}")
+      }
+    }
+  }
+
+  def runCreeps = Memory.flatMap { mem ⇒
+    Game.flatMap { game ⇒
+      if (!js.isUndefined(mem.workers)) {
+        val workers = mem.workers.as[js.Dictionary[Creep]]
+        workers.toList.zipWithIndex.map {
+          case ((name, _), i) ⇒
+            val creep = game.creeps(name)
+            if (i < 2)
+              build(creep)
+            else if (i < 4)
+              harvest(creep)
+            else
+              upgrade(creep)
+        }.foldLeft(Task.unit) {
+          case (p, t) ⇒ p.flatMap(_ ⇒ t)
+        }
+      } else Task.unit
+    }
+  }
 
   def doWork(creep: Creep, workingPhrase: String)(f: Creep ⇒ Unit): Task[Unit] = Memory.map { mem ⇒
     def updateSources(creep: Creep) = {
@@ -70,7 +129,7 @@ object Tasks {
         storage.structureType == STRUCTURE_STORAGE &&
           storage.store(RESOURCE_ENERGY) >= 0
       }
-      })).headOption.map { target ⇒
+      })).headOption.fold (up(creep)) { target ⇒
         if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) creep.moveTo(target)
       }
       0
@@ -80,9 +139,18 @@ object Tasks {
     }
   }
 
-  def upgrade(c: Creep): Task[Unit] = doWork(c, "upgrading") { creep ⇒
+  def up(creep: Creep): Unit = {
     if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
       creep.moveTo(creep.room.controller)
+    }
+  }
+
+  def upgrade(c: Creep): Task[Unit] = doWork(c, "upgrading")(up)
+
+  def build(c: Creep): Task[Unit] = doWork(c, "building") { creep ⇒
+    val targets = creep.room.find(FIND_CONSTRUCTION_SITES, FindFilter[ConstructionSite](_ ⇒ true))
+    targets.sortWith(_.progress > _.progress).headOption.fold (up(creep)) { site ⇒
+      if (creep.build(site) == ERR_NOT_IN_RANGE) creep.moveTo(site)
     }
   }
 }
